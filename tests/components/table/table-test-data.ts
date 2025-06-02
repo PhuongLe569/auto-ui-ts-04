0fe164e3a80b4e89988892e6f173dd43bf35e81d
PhuongLe569 <phhle0609@gmail.com>
2025-06-02 19:54:51 +0700

    [US-15] Implement test for table

---
+++ tests/components/table/table-test-data.ts
@@ -0,0 +1,137 @@
+export const expectedTableData = 
+[
+  {
+    "Name": "John Brown",
+    "Address": "New York No. 1 Lake Park",
+    "Tags": [
+      "NICE",
+      "DEVELOPER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Green",
+    "Address": "London No. 1 Lake Park",
+    "Tags": [
+      "LOSER"
+    ],
+    "Age": "42"
+  },
+  {
+    "Name": "Joe Black",
+    "Address": "Sidney No. 1 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Red",
+    "Address": "London No. 2 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Blue",
+    "Address": "London No. 3 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Yellow",
+    "Address": "London No. 4 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Pink",
+    "Address": "London No. 5 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Orange",
+    "Address": "London No. 6 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Purple",
+    "Address": "London No. 7 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Brown",
+    "Address": "London No. 8 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Green",
+    "Address": "London No. 9 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Black",
+    "Address": "London No. 10 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Red",
+    "Address": "London No. 11 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Blue",
+    "Address": "London No. 12 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  },
+  {
+    "Name": "Jim Yellow",
+    "Address": "London No. 13 Lake Park",
+    "Tags": [
+      "COOL",
+      "TEACHER"
+    ],
+    "Age": "32"
+  }
+];
